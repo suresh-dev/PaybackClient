3663b6ad40a4123541b09731c707974fb4139f5d
suresh-dev <sure.cse@gmail.com>
2019-04-15 08:05:41 +0530

    Add project files.

---
+++ PAYBACK.App/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using PAYBACK.App.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PAYBACK.App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private IPayBackService payBackService;
+        public MainWindow()
+        {
+            InitializeComponent();
+            payBackService = new PayBackService();
+        }
+
+        private void BtnVerify_Click(object sender, RoutedEventArgs e)
+        {
+            Error.Content = payBackService.GetAccountBalance();
+        }
+    }
+}
---
+++ PAYBACK.App/Services/PayBackService.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PAYBACK.App.Services
+{
+    public class PayBackService : IPayBackService
+    {
+        private string token;
+        private PayBackServiceReference.ExtintPortTypeClient GetClient()
+        {
+            return new PayBackServiceReference.ExtintPortTypeClient("ExtintSoapBinding_ExtintPortType");
+        }
+        public string Authenticate()
+        {
+            try
+            {
+
+                var authRequest = new PayBackServiceReference.AuthenticateRequest
+                {
+                    Authentication = new PayBackServiceReference.MemberAliasAuthenticationType
+                    {
+                        Identification = new PayBackServiceReference.MemberIdentificationType
+                        {
+                            Alias = "9966108838",
+                            AliasType = PayBackServiceReference.PrincipalVariantType.Item3,
+                            AliasTypeSpecified = true
+                        },
+                        Security = new PayBackServiceReference.MemberSecurityType
+                        {
+                            SecretType = PayBackServiceReference.PrincipalSecretType.Item0
+                        }
+                    },
+                    CommunicationChannel = "1"
+                };
+
+                //var ress = GetClient().GetMemberCardDetailsByMobile(new PayBackServiceReference.GetMemberCardDetailsByMobileRequest
+                //{
+                //    MobileNumber = "9966108838"
+                //});
+                var res = GetClient().Authenticate(authRequest);
+
+                if (!string.IsNullOrEmpty(res.Token))
+                    token = res.Token;
+                return string.Empty;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        public string GetAccountBalance()
+        {
+            try
+            {
+
+                var req = new PayBackServiceReference.GetAccountBalanceRequest
+                {
+                    Authentication = new PayBackServiceReference.AuthenticationType
+                    {
+                        Principal = new PayBackServiceReference.PrincipalType
+                        {
+                            PrincipalValue = "9966108838",
+                            PrincipalClassifier = "3"
+                        },
+                        Credential = "7282"
+                    },
+                    SendLoyCardNo = PayBackServiceReference.StatusType.YES,
+
+                };
+
+                var res = GetClient().GetAccountBalance(req);
+                if (res.AccountBalanceDetails != null)
+                    return string.Format("Total Points: {0}", res.AccountBalanceDetails.TotalPointsAmount);
+                return string.Empty;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
